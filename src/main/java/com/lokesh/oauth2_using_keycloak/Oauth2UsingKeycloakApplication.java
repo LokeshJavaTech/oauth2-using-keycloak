3842d937c07c88cbcfda5c50c04087464dce629b
Lokesh Mittal <lokesh.adonis@gmail.com>
2025-02-19 17:42:42 -0500

    Empty spring boot project - Having Spring Web and Lombok dependency

---
+++ src/main/java/com/lokesh/oauth2_using_keycloak/Oauth2UsingKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.lokesh.oauth2_using_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2UsingKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2UsingKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lokesh/oauth2_using_keycloak/Oauth2UsingKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokesh.oauth2_using_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2UsingKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
